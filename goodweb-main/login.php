0a1461f7b2f9866f349604a7df914bc1bffb646f
shashankkr46 <shashankkr46@gmail.com>
2019-12-04 12:10:40 +0530

    intense

---
+++ goodweb-main/login.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <!-- Basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+   
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+ 
+     <!-- Site Metas -->
+    <title>National Public Welfare Mission</title>  
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Site CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!-- Modernizer for Portfolio -->
+    <script src="js/modernizer.js"></script>
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+
+    <!-- LOADER -->
+    <div id="preloader">
+        <div class="loader">
+            <div class="loader__bar"></div>
+            <div class="loader__bar"></div>
+            <div class="loader__bar"></div>
+            <div class="loader__bar"></div>
+            <div class="loader__bar"></div>
+            <div class="loader__ball"></div>
+        </div>
+    </div><!-- end loader -->
+    <!-- END LOADER -->
+    <header class="header header_style_01">
+        <nav class="megamenu navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html"><img src="images/logos/logo1.jpg" alt="image"></a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right menu">
+                        <li><a class="active" href="index.html">Home</a></li>
+                        <li class="dropdown">
+                          <a href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About us</a>
+                          <ul class="dropdown-menu sub">
+                            <li><a href="#" class="color">Overview</a></li>
+                            <li><a href="mission.html" class="color">Mission,Vision and Values</a></li>
+                            <li><a href="team.html" class="color">Our Team</a></li>
+                          </ul>
+                        </li>
+                        <li class="dropdown">
+                          <a href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services </a>
+                          <ul class="dropdown-menu">
+                            <li><a href="#" class="color">Nursery and Primary Education</a></li>
+                            <li><a href="#" class="color">Health</a></li>
+                            <li><a href="#" class="color">Social Awareness Programme</a></li>
+                            <li><a href="#" class="color">Hardware and Software Training</a></li>
+                            <li><a href="#" class="color">Placement</a></li>
+                            <li><a href="#" class="color">Skill Development</a></li>
+                          </ul>
+                        </li>
+                        <li><a href="#" data-toggle="modal" data-target="#exampleModal">Donate</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                        <li><a href="">Login</a></li>
+                       
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+   
+   
+    <div class="parallax section lb">
+        <div class="container cont-login">
+            
+           <div class="row">
+                <div class=" col-lg-12 login-reg-panel">
+                <div class="login-info-box">
+                    <h2>Have an account?</h2>
+                    <p>Lorem ipsum dolor sit amet</p>
+                    <label id="label-register" for="log-reg-show">Login</label>
+                    <input type="radio" name="active-log-panel" id="log-reg-show"  checked="checked">
+                </div>
+                                    
+                <div class="register-info-box">
+                    <h2>Don't have an account?</h2>
+                    
+                    <label id="label-login" for="log-login-show">Register</label>
+                    <input type="radio" name="active-log-panel" id="log-login-show">
+                </div>
+                                    
+                <div class="white-panel">
+                    <div class="login-show log-space">
+                        <h2>LOGIN</h2>
+                        <input type="text" placeholder="Email">
+                        <input type="password" placeholder="Password">
+                        <input type="button" value="Login">
+                        <a href="">Forgot password?</a>
+                    </div>
+                    <div class="register-show reg-space">
+                        <h2>REGISTER</h2>
+                        <input type="text" placeholder="Email">
+                        <input type="password" placeholder="Password">
+                        <input type="password" placeholder="Confirm Password">
+                        <input type="button" value="Register">
+                    </div>
+                </div>
+        </div>
+            </div>
+           
+        </div>
+    </div><!-- end section -->
+
+    
+    
+    
+
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="widget clearfix">
+                        <div class="widget-title">
+                            <img src="images/logos/logo1.jpg"   alt="" />
+                        </div>
+                        <p> Integer rutrum ligula eu dignissim laoreet. Pellentesque venenatis nibh sed tellus faucibus bibendum. Sed fermentum est vitae rhoncus molestie. Cum sociis natoque penatibus et magnis dis montes.</p>
+                        <p>Sed fermentum est vitae rhoncus molestie. Cum sociis natoque penatibus et magnis dis montes.</p>
+                    </div><!-- end clearfix -->
+                </div><!-- end col -->
+
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="widget clearfix">
+                        <div class="widget-title">
+                            <h3>Pages</h3>
+                        </div>
+
+                        <ul class="footer-links hov">
+                            <li><a href="#">Home <span class="icon icon-arrow-right2"></span></a></li>
+                            <li><a href="#">Blog <span class="icon icon-arrow-right2"></span></a></li>
+                            <li><a href="#">Pricing <span class="icon icon-arrow-right2"></span></a></li>
+                            <li><a href="#">About <span class="icon icon-arrow-right2"></span></a></li>
+                            <li><a href="#">Faq <span class="icon icon-arrow-right2"></span></a></li>
+                            <li><a href="#">Contact <span class="icon icon-arrow-right2"></span></a></li>
+                        </ul><!-- end links -->
+                    </div><!-- end clearfix -->
+                </div><!-- end col -->
+                
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="footer-distributed widget clearfix">
+                        <div class="widget-title">
+                            <h3>Subscribe</h3>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which one know this tricks.</p>
+                        </div>
+                        
+                        <div class="footer-right">
+                            <form method="get" action="#">
+                                <input placeholder="Subscribe our newsletter.." name="search">
+                                <i class="fa fa-envelope-o"></i>
+                            </form>
+                        </div>                        
+                    </div><!-- end clearfix -->
+                </div><!-- end col -->
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </footer><!-- end footer -->
+
+    <div class="copyrights">
+        <div class="container">
+            <div class="footer-distributed">
+                <div class="footer-left">                   
+                    <p class="footer-company-name">All Rights Reserved. &copy; 2018 <a href="#">Intense Global Solution</a> Design By : 
+                    <a href="https://html.design/">html design</a></p>
+                </div>
+
+                
+            </div>
+        </div><!-- end container -->
+    </div><!-- end copyrights -->
+
+    <a href="#" id="scroll-to-top" class="dmtop global-radius"><i class="fa fa-angle-up"></i></a>
+
+    <!-- ALL JS FILES -->
+    <script src="js/all.js"></script>
+    <!-- ALL PLUGINS -->
+    <script src="js/custom.js"></script>
+    <script src="js/portfolio.js"></script>
+    <script src="js/hoverdir.js"></script>   
+    <script src="js/ani.js"></script> 
+    <script type="text/javascript">
+         $(document).ready(function(){
+    $('.login-info-box').fadeOut();
+    $('.login-show').addClass('show-log-panel');
+});
+
+
+$('.login-reg-panel input[type="radio"]').on('change', function() {
+    if($('#log-login-show').is(':checked')) {
+        $('.register-info-box').fadeOut(); 
+        $('.login-info-box').fadeIn();
+        
+        $('.white-panel').addClass('right-log');
+        $('.register-show').addClass('show-log-panel');
+        $('.login-show').removeClass('show-log-panel');
+        
+    }
+    else if($('#log-reg-show').is(':checked')) {
+        $('.register-info-box').fadeIn();
+        $('.login-info-box').fadeOut();
+        
+        $('.white-panel').removeClass('right-log');
+        
+        $('.login-show').addClass('show-log-panel');
+        $('.register-show').removeClass('show-log-panel');
+    }
+});
+  
+
+    </script>
+
+</body>
+</html>
